e6d8722f1d2fdeb9127d694374a786df46d8a4f5
Fred Kesse <fredkesse1234@gmail.com>
2023-02-13 11:42:59 +0000

    exo de palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,5 @@
+# Ecrire une fonction qui permet de vérifier si une chaine de caractère est un palindrome
+
 def palindrome(v):
     reverse = v[::-1]
     if v==reverse:
@@ -6,8 +8,8 @@ def palindrome(v):
 
 mot = input("Entrer un mot : ")
 if palindrome(mot):
-    print("Ce mot un un palindrome")
+    print(f"{mot} un un palindrome")
 
 else:
-    print("Ce mot n'est pas un palindrome")
+    print(f"{mot} n'est pas un palindrome")
 
