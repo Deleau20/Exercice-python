a0c86827e485f5babd48914a8073d33a1282ae03
Fred Kesse <fredkesse1234@gmail.com>
2023-02-13 11:24:13 +0000

    exo de tableau carre

---
+++ Fonction.py
@@ -0,0 +1,10 @@
+# Ecrire une fonction qui prend en parametre un tableau qui
+# retourne un autre tableau contenent le carré des nombre entrée.
+
+def carre(num):
+    return num**2  
+
+nombre =[1, 2, 3, 4]
+for item in map(carre, nombre):
+    list(map(carre,nombre))
+    print(item) 
